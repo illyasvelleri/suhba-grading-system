f281e560cf84f95965cc66bb31f257826193bbc4
illyas velleri <cookon123@gmail.com>
2025-03-03 05:57:08 -0500

    admni can view complete editable table

--- models/Table.js
+++ models/Table.js
@@ -1,49 +1,66 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const tableSchema = new mongoose.Schema({
-    section: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Section',
-        required: true
-    },
-    tableName: {
-        type: String,
-        required: true
-    },
-    columns: [
-        {
-            name: {
-                type: String,
-                required: true
+const tableSchema = new mongoose.Schema(
+    {
+        section: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Section",
+            required: true,
+        },
+        tableName: {
+            type: String,
+            required: true,
+        },
+        columns: [
+            {
+                name: {
+                    type: String,
+                    required: true,
+                },
+                type: {
+                    type: String,
+                    enum: ["text", "radio", "marks", "max-marks"], // Column Type
+                    required: true,
+                },
+                isEditable: {
+                    type: Boolean,
+                    default: false,
+                },
             },
-            type: {
-                type: String,
-                enum: ['text', 'radio', 'marks'],
-                required: true
+        ],
+        rowsCount: {
+            type: Number,
+            required: true,
+        },
+        data: [
+            {
+                rowNumber: Number,
+                columns: [
+                    {
+                        columnName: String,
+                        value: String, // Editable Value Here
+                        type: {
+                            type: String,
+                            enum: ["text", "radio", "marks", "max-marks"], // Type Store in Data Also
+                        },
+                    },
+                ],
             },
-            isEditable: {
-                type: Boolean,
-                default: false
-            }
+        ],
+        totalMarks: {
+            type: Number,
+            default: 0, // Automatically Calculate Total Marks
+        },
+        maxMarks: {
+            type: Number,
+            default: 0, // Automatically Calculate Max Marks
+        },
+        percentage: {
+            type: Number,
+            default: 0, // Automatically Calculate Percentage
         }
-    ],
-    rowsCount: {
-        type: Number,
-        required: true
     },
-    data: [
-        {
-            rowNumber: {
-                type: Number
-            },
-            columns: [
-                {
-                    columnName: String,
-                    value: String
-                }
-            ]
-        }
-    ]
-}, { timestamps: true });
+    { timestamps: true }
+);
 
-module.exports = mongoose.model('Table', tableSchema);
+module.exports = mongoose.model("Table", tableSchema);
